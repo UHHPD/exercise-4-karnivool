84bd2e9b3f341d4da3f6ec94d058f65fb90b15ef
TobiFreund <tobias.j.freund@gmail.com>
2023-11-29 16:55:22 +0100

    Ausgabe von Bin 27 (Aufgabe 1c)

--- run.cc
+++ run.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <functional>
 #include <string>
+#include <cmath>
 
 #include "Data.hh"
 
@@ -65,6 +66,27 @@ int main() {
 
   cout << "******************************************************" << endl;
   runTests();
+  cout << "******************************************************" << endl;
+
+  vector<Data> datasets;
+  vector<string> datasetnames = {"exp_A", "exp_B", "exp_C", "exp_D"};
+
+  for(string dsname : datasetnames)
+    datasets.push_back(Data(dsname));
+
+  for(int i=0; i<datasets.size(); i++) {
+    cout << "Crosssection in bin 27 for experiment ";
+    cout << datasetnames[i] << ": " << datasets[i].measurement(27) << endl;
+
+    Data ds1 = datasets[i];
+    Data ds2 = datasets[(i+1) % 4];
+    double sigma = sqrt(pow(ds1.error(27), 2) + pow(ds2.error(27), 2));
+
+    cout << "Difference to next dataset: ";
+    cout << ds2.measurement(27) - ds1.measurement(27) << endl;
+    cout << "sigma: " << sigma << ", 2 sigma: " << 2*sigma << endl;
+  }
+
   cout << "******************************************************" << endl;
   // create an object which holds data of experiment A
   Data datA("exp_A");
